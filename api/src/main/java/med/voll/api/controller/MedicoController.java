9ef1a7f875220c845a273767a2aedfe03bfe7c9c
Maria Ritha Nascimento <maria.ritha.2000@gmail.com>
2023-07-31 18:37:56 -0300

    Criação da ListagemMedico.

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,12 +1,16 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import jakarta.validation.Valid;
 import med.voll.api.medicos.DadosCadastroMedico;
+import med.voll.api.medicos.DadosListagemMedico;
 import med.voll.api.medicos.Medico;
 import med.voll.api.medicos.MedicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +25,12 @@ public class MedicoController {
 	@PostMapping
 	@Transactional
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-
 		repository.save(new Medico(dados));
+	}
+
+	@GetMapping
+	public List<DadosListagemMedico> listagem(){
+		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
 	}
 
 }
