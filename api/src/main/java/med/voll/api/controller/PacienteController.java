b118c7d1402d161081482df0f3ef6fb68b6f02c5
Maria Ritha Nascimento <maria.ritha.2000@gmail.com>
2023-07-31 19:26:42 -0300

    Alteração no PacienteController

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,12 +1,18 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medicos.DadosListagemMedico;
 import med.voll.api.pacientes.DadosCadastroPaciente;
+import med.voll.api.pacientes.DadosListagemPaciente;
 import med.voll.api.pacientes.Paciente;
 import med.voll.api.pacientes.PacienteRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +28,10 @@ public class PacienteController {
 	@Transactional
 	public void cadastrarPacientes(@RequestBody @Valid DadosCadastroPaciente dados){
 		repository.save(new Paciente(dados));
-
-
+		}
+	@GetMapping
+	public Page<DadosListagemPaciente> listagem(@PageableDefault(size = 1, sort = {"nome"}) Pageable paginacao){
+		return repository.findAll(paginacao).map(DadosListagemPaciente::new);
 	}
-}
+	}
+
