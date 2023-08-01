85b37356d594c92ffb3c75e762a903e4904d67d0
Maria Ritha Nascimento <maria.ritha.2000@gmail.com>
2023-08-01 20:46:14 -0300

    Realizada implementação do método DELETE na modalidade Médicos.

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -12,7 +12,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,12 +35,19 @@ public class MedicoController {
 
 	@GetMapping
 	public Page<DadosListagemMedico> listagem(@PageableDefault(size = 5, sort = {"crm"})Pageable paginacao){
-		return repository.findAll(paginacao).map(DadosListagemMedico::new);
+		return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
 	}
 	@PutMapping
 	@Transactional
 	public void atualizar(@RequestBody @Valid DadosAtualizaMedico dados){
 		var medico = repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluir(@PathVariable Long id){
+		var medico = repository.getReferenceById(id);
+		medico.excluir();
 	}
 }
